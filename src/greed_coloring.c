a3681bd1af3935b32901bbeb02ece8fb2a9d2e30
João Almeida Jr <joao29a@gmail.com>
2014-02-16 08:04:03 -0300

    better now

--- src/greed_coloring.c
+++ src/greed_coloring.c
@@ -4,21 +4,27 @@ int compare(vertex_node_t* vertex1, vertex_node_t* vertex2){
     return vertex1->edges_size > vertex2->edges_size;
 }
 
-/* this shit is ugly as fuck, but it works hard as fuck */
-/* Greedy Algorithm + LDO */
-size_t greed_coloring(Graph* graph){
+/* O(2V) */
+vertex_node_t** largest_degree_ordering(Graph* graph, size_t* len){
     vertex_node_t** heapq = (vertex_node_t**) create_heap(1024);
-    size_t heapq_len = 0;
     
     hash_iterator_t* j = graph->begin;
     while (j != NULL){
-        heapq[heapq_len++] = j->hash_node->value;
+        heapq[*len] = j->hash_node->value;
+        *len += 1;
         j = j->next;
     }
 
-    build_heap((void**) heapq, heapq_len, (int(*)(void*,void*)) &compare);
+    build_heap((void**) heapq, *len, (int(*)(void*,void*)) &compare);
 
-    size_t used_len = 0;
+    return heapq;
+}
+
+/* Greedy Algorithm + LDO  O(V*E)  */
+size_t greed_coloring(Graph* graph){
+    size_t heapq_len = 0, used_len = 1;
+    vertex_node_t** heapq = largest_degree_ordering(graph, &heapq_len);
+
     hash_table_t* adj_colors = create_hash_table(heapq_len);
     char color[20];
 
@@ -35,27 +41,16 @@ size_t greed_coloring(Graph* graph){
                 set_hash(adj_colors, color, vertex->name);
             }
         }
-
-        if (empty){
-            vertex->color = 0;
-            if (used_len == 0) used_len++;
-            continue;
-        }
-
         for (i = 0; i < used_len; i++){
             sprintf(color,"%d",i);
             hash_node_t* adj_color = get_hash(adj_colors, color);
-
-            if (adj_color == NULL){
-                vertex->color = i;
-                break;
-            }
-            else if (strcmp((char*) adj_color, vertex->name) != 0){
+            
+            if (adj_color == NULL || 
+                    strcmp((char*) adj_color, vertex->name) != 0){
                 vertex->color = i;
                 break;
             }
         }
-
         if (i == used_len){
             vertex->color = used_len;
             used_len++;
