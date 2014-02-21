84ad632fa8448bd787a2893d893c263e99c6e9d5
João Almeida Jr <joao29a@gmail.com>
2014-02-20 21:15:29 -0300

    script to get the hardware performance

--- makefile
+++ makefile
@@ -1,2 +1,4 @@
-all:
+all: compile
+
+compile:
 	clang -g -Wall -Wextra src/*.c -o out -I./src/hdr/ -std=c11
--- testall.sh
+++ testall.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
-time for i in $(find ./input/ -type f); do echo $i; time ./out $i; echo; done
+get_time(){
+    time for i in $(find ./input/ -type f); do 
+        echo $i; time ./out $i; 
+        echo; 
+    done
+}
+
+perf(){
+    for i in $(find ./input/ -type f); do
+        psrun -c hardware/system.xml -o $i -F text ./out $i -p
+        mv ./input/*.txt ./
+        for file in $(find . -name *.txt -type f); do
+            cat $file | grep -A21 Index | grep -v -e = -e Index > temp
+            rm $file
+            i=$(echo $i | sed -e s/.input// -e s/.//)
+            mkdir -p hardware/result$i
+            touch hardware/result$i/$file
+            while read -r fileDes <&1 && read -r fileTxt <&2; do
+                fileTxt=$(echo $fileTxt | cut -d " " -f3)
+                echo -e "$fileDes $(printf "%'d" $fileTxt)" >> hardware/result$i/$file
+            done 1<hardware/description.md 2<temp
+            rm temp
+        done
+    done
+}
+
+clean(){
+    for i in $(find ./input/*.txt -type f); do
+        new=$(echo $i | sed -e s/.txt//)
+        mv $i $new
+    done
+}
+
+$1
