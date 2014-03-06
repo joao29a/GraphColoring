ce7ba0a009f33ae463c04fdba7efa96cb40ad0ef
João Almeida Jr <joao29a@gmail.com>
2014-03-06 17:27:02 -0300

    test one or all

--- testall.sh
+++ testall.sh
@@ -2,30 +2,39 @@
 
 prog=color.out
 
-get_time(){
+time-all(){
     time for i in $(find ./input/ -type f); do 
-        echo $i; time ./$prog $i; 
-        echo; 
+        time-one $i
+    done
+}
+
+time-one(){
+    echo $1; time ./$prog $1; 
+    echo; 
+}
+
+perf-all(){
+    for i in $(find ./input/ -type f); do
+        perf $1 $i
     done
 }
 
 perf(){
-    for i in $(find ./input/ -type f); do
-        psrun -c $1.xml -o $i -F text ./$prog $i
-        mv ./input/*.txt ./
-        for file in $(find . -name *.txt -type f); do
-            total_lines=$(cat $1.md | wc -l)
-            cat $file | grep -A$total_lines Index | grep -v -e = -e Index > temp
-            rm $file
-            i=$(echo $i | sed -e s/.input// -e s/.//)
-            mkdir -p hardware/result$i
-            touch hardware/result$i/$file
-            while read -r fileDes <&1 && read -r fileTxt <&2; do
-                fileTxt=$(echo $fileTxt | cut -d " " -f3)
-                echo -e "$fileDes $(printf "%'d" $fileTxt)" >> hardware/result$i/$file
-            done 1<$1.md 2<temp
-            rm temp
-        done
+    psrun -c $1.xml -o $2 -F text ./$prog $2
+    mv ./input/*.txt ./
+    for file in $(find . -name *.txt -type f); do
+        let total_lines=$(cat $1.md | wc -l)+1
+        cat $file | grep -A$total_lines Index | grep -v -e = -e Index > temp
+        rm $file
+        i=$(echo $2 | sed -e s/.input// -e s/.//)
+        i=$(echo $i | sed -e s/nput//)
+        mkdir -p hardware/result$i
+        touch hardware/result$i/$file
+        while read -r fileDes <&1 && read -r fileTxt <&2; do
+            fileTxt=$(echo $fileTxt | cut -d " " -f3)
+            echo -e "$fileDes $(printf "%'d" $fileTxt)" >> hardware/result$i/$file
+        done 1<$1.md 2<temp
+        rm temp
     done
 }
 
@@ -36,4 +45,4 @@ clean(){
     done
 }
 
-$1 $2
+$1 $2 $3
