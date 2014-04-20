287b48ea709c89d4166e20a4edc6ab0ae3cdc98b
João Almeida Jr <joao29a@gmail.com>
2014-04-20 02:40:16 -0300

    script to count the total time in seconds

---
+++ results/total-time.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file=$1
+total_ms=0
+total_sec=0
+total_min=0
+while read i; do
+    time=$(echo $i | cut -d '(' -f2 | cut -d ')' -f1)
+    ms=$(echo $time | cut -d ' ' -f3 | sed -e s/[a-z]//g)
+    sec=$(echo $time | cut -d ' ' -f2 | sed -e s/[a-z]//g)
+    min=$(echo $time | cut -d ' ' -f1 | sed -e s/[a-z]//g)
+    let total_ms=$total_ms+$ms
+    let total_sec=$total_sec+$sec
+    let total_min=$total_min+$min
+done < $file
+let ms_sec=$total_ms/1000
+let min_sec=$total_min*60
+let total_sec=$total_sec+$min_sec+$ms_sec
+echo "$total_sec seconds"
