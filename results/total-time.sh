6b927aa8c9e95a9055cee6bc4274df175b35a7da
João Almeida Jr <joao29a@gmail.com>
2014-04-20 15:57:23 -0300

    parallel with 4 threads log

--- results/total-time.sh
+++ results/total-time.sh
@@ -4,7 +4,10 @@ file=$1
 total_ms=0
 total_sec=0
 total_min=0
+total_colors=0
 while read i; do
+    colors=$(echo $i | cut -d ' ' -f5)
+    let total_colors=$total_colors+$colors
     time=$(echo $i | cut -d '(' -f2 | cut -d ')' -f1)
     ms=$(echo $time | cut -d ' ' -f3 | sed -e s/[a-z]//g)
     sec=$(echo $time | cut -d ' ' -f2 | sed -e s/[a-z]//g)
@@ -17,3 +20,4 @@ let ms_sec=$total_ms/1000
 let min_sec=$total_min*60
 let total_sec=$total_sec+$min_sec+$ms_sec
 echo "$total_sec seconds"
+echo "$total_colors colors"
